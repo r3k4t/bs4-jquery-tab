774a0929f5e865e931827f14a53910b75faa4ae6
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2022-05-04 13:02:43 +0600

    Add files via upload

---
+++ js/jquery.tab-hide.bs.tab.js
@@ -0,0 +1,14 @@
+$(function(){
+  $(".btn-primary").click(function(){
+    $('.toast').toast('show');
+  });
+
+  $('.toast').on('show.bs.toast', function(){
+    alert('The toast is about to be shown.');
+  });
+  $('.toast').on('shown.bs.toast', function(){
+    alert('The toast is now fully shown.');
+  });
+  $('.toast').on('hide.bs.toast', function(){
+    alert('The toast is about to be hidden.');
+  });
